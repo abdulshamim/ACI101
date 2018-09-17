0a1b93acf9865f85ba3f1f1a327d01d0fa501d7d
Abdul <abdul.shamimkhan5@gmail.com>
2018-09-18 01:11:15 +0530

    AppCenter frame works added

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,6 +1,15 @@
+
 import UIKit
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterPush
+
 
 @UIApplicationMain
+
+//
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
     static var entries: FileEntryStore = FileEntryStore()
@@ -9,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        MSAppCenter.start("f462e91e-8aea-4ec6-8e3f-eb84e8a812f2", withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
         return true
     }
 
---
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -0,0 +1,42 @@
+//
+//  MinutesTests.swift
+//  MinutesTests
+//
+//  Created by Abdul Shamim on 18/09/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import Minutes
+
+class MinutesTests: XCTestCase {
+    
+    var entryTest: Entry!
+    
+    override func setUp() {
+        super.setUp()
+        
+        entryTest = Entry("Title", "Content")
+    }
+    
+    override func tearDown() {
+        entryTest = nil
+        super.tearDown()
+    }
+    
+    func testSerialization() {
+        let serializeData = FileEntryStore.serialize(entryTest)
+        let entry = FileEntryStore.deserialize(serializeData!)
+        
+        XCTAssertEqual(entry?.title, "Title", "Title does not match")
+        XCTAssertEqual(entry?.content, "Content", "Content does not match")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
